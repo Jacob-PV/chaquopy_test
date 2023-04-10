56494ebac4c42e8d6d821b342756e24c67cfacee
jpvf2d <jpvf2d@gmail.com>
2023-04-10 18:51:00 -0500

    can run preprocess graph

--- app/src/main/java/com/chaquo/myapplication/MainActivity.kt
+++ app/src/main/java/com/chaquo/myapplication/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.chaquo.myapplication
 
-import android.content.Context
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.view.inputmethod.InputMethodManager
+import android.util.Log
 import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.chaquo.python.PyException
@@ -26,20 +22,34 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<Button>(R.id.button).setOnClickListener {
             try {
-                val bytes = module.callAttr("plot",
-                                            findViewById<EditText>(R.id.etX).text.toString(),
-                                            findViewById<EditText>(R.id.etY).text.toString())
-                    .toJava(ByteArray::class.java)
-                val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-                findViewById<ImageView>(R.id.imageView).setImageBitmap(bitmap)
-
-                currentFocus?.let {
-                    (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
-                        .hideSoftInputFromWindow(it.windowToken, 0)
-                }
+                val pythonInput = intArrayOf(1, 2, 3, 4)
+                val bytes = module.callAttr("main", pythonInput)
+                Log.d("ptj", bytes.toString())
+//                    .toJava(ByteArray::class.java)
+//                val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+//                findViewById<ImageView>(R.id.imageView).setImageBitmap(bitmap)
+//                println(bitmap.toString())
+//                currentFocus?.let {
+//                    (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+//                        .hideSoftInputFromWindow(it.windowToken, 0)
+//                }
             } catch (e: PyException) {
                 Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
             }
         }
     }
+}
+
+
+if (! Python.isStarted()) {
+    Python.start(AndroidPlatform(this))
+}
+val py = Python.getInstance()
+val module = py.getModule("plot")
+try {
+    val pythonInput = intArrayOf(1, 2, 3, 4)
+    val bytes = module.callAttr("main", pythonInput)
+    Log.d("ptj", bytes.toString())
+} catch (e: PyException) {
+    Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
 }
--- app/src/main/python/plot.py
+++ app/src/main/python/plot.py
@@ -1,13 +1,49 @@
-import io
-import matplotlib.pyplot as plt
+import networkx as nx
+import scipy.sparse as sp
+import numpy as np
+import torch
 
-def plot(x, y):
-    xa = [float(word) for word in x.split()]
-    ya = [float(word) for word in y.split()]
 
-    fig, ax = plt.subplots()
-    ax.plot(xa, ya)
+def main(locations):
+    environment = nx.Graph()
 
-    f = io.BytesIO()
-    plt.savefig(f, format="png")
-    return f.getvalue()
+    for v1 in locations:
+        for v2 in locations:
+            environment.add_edge(v1, v2)
+
+    environment =  nx.adjacency_matrix(environment)
+
+    # print(environment)
+    adj = sp.csr_matrix(environment)
+    # print(adj.toarray())
+    adj_norm = preprocess_graph(adj)
+    print("HERE")
+    print(adj_norm)
+    return adj_norm
+
+
+def preprocess_graph(adj):
+    adj = sp.coo_matrix(adj)
+    adj_ = adj + sp.eye(adj.shape[0])
+    # print(adj_)
+    # exit()
+    rowsum = np.array(adj_.sum(1))
+    degree_mat_inv_sqrt = sp.diags(np.power(rowsum, -0.5).flatten())
+    adj_normalized = adj_.dot(degree_mat_inv_sqrt).transpose().dot(degree_mat_inv_sqrt).tocoo()
+    # return sparse_to_tuple(adj_normalized)
+    return sparse_mx_to_torch_sparse_tensor(adj_normalized)
+
+
+def sparse_mx_to_torch_sparse_tensor(sparse_mx):
+    """Convert a scipy sparse matrix to a torch sparse tensor."""
+    sparse_mx = sparse_mx.tocoo().astype(np.float32)
+    indices = torch.from_numpy(
+        np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+    values = torch.from_numpy(sparse_mx.data)
+    shape = torch.Size(sparse_mx.shape)
+    return torch.sparse.FloatTensor(indices, values, shape)
+
+# def hello(locations):
+# #     locations = [1,2,3,4]
+#     print("LOC", locations)
+#     return main(locations)
